799e594023d119ce1caa0a0d1a9b9c475f09a3c6
fifi <filipn924@gmail.com>
2022-12-11 15:20:11 +0000

    feat: able to access index.html + docker

--- fifinest/fifinest/urls.py
+++ fifinest/fifinest/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('fifinest/', include('fifinest_app.urls')),
+    path('', include('fifinest_app.urls')),
     path("admin/", admin.site.urls),
 ]
